2e81504336d88b67d710cbcec17d6609b0c9faaf
khalidrahmanhanify <khalidrahmanhanify@gmail.com>
2026-03-29 20:04:15 +0430

    Optmized images

--- src/content.config.ts
+++ src/content.config.ts
@@ -19,31 +19,32 @@ const notes = defineCollection({
 
 const semesters = defineCollection({
   loader: file("src/data/semesters.json"),
-  schema: z.object({
-    semesterName: z.string(),
-    slug: z.string(),
-    url: z.string(),
-    credits: z.number(),
-    barColor: z.string(),
-    subjects: z.array(
-      z.object({
-        subjectName: z.string(),
-        coverImage: z.string(),
-        iconName: z.string(),
-        teacher: z.string(),
-        location: z.string(),
-        day: z.string(),
-        path: z.string(),
-        materials: z.array(
-          z.object({
-            title: z.string(),
-            file: z.string(),
-            date: z.coerce.date().optional(),
-          }),
-        ),
-      }),
-    ),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      semesterName: z.string(),
+      slug: z.string(),
+      url: z.string(),
+      credits: z.number(),
+      barColor: z.string(),
+      subjects: z.array(
+        z.object({
+          subjectName: z.string(),
+          coverImage: image(),
+          iconName: z.string(),
+          teacher: z.string(),
+          location: z.string(),
+          day: z.string(),
+          path: z.string(),
+          materials: z.array(
+            z.object({
+              title: z.string(),
+              file: z.string(),
+              date: z.coerce.date().optional(),
+            }),
+          ),
+        }),
+      ),
+    }),
 });
 
 export const collections = { notes, semesters };
